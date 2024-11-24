0d135591a185dcf6784f39e1e11e3c94f9955752
ulyagff <ulyagff@gmail.com>
2024-11-24 20:19:40 +0300

    закомментила для тестов

--- src/main/kotlin/ru/quipy/projections/AnnotationBasedStatusEventsSubscriber.kt
+++ src/main/kotlin/ru/quipy/projections/AnnotationBasedStatusEventsSubscriber.kt
@@ -1,28 +1,28 @@
-package ru.quipy.projections
-
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import org.springframework.stereotype.Service
-import ru.quipy.api.StatusCreatedEvent
-import ru.quipy.api.StatusUpdatedEvent
-import ru.quipy.streams.annotation.AggregateSubscriber
-import ru.quipy.streams.annotation.SubscribeEvent
-
-@Service
-@AggregateSubscriber(
-    aggregateClass = StatusAggregate::class, subscriberName = "status-subs-stream"
-)
-class AnnotationBasedStatusEventsSubscriber {
-
-    val logger: Logger = LoggerFactory.getLogger(AnnotationBasedStatusEventsSubscriber::class.java)
-
-    @SubscribeEvent
-    fun statusCreatedSubscriber(event: StatusCreatedEvent) {
-        logger.info("Status created: {} with color {}", event.statusName, event.color)
-    }
-
-    @SubscribeEvent
-    fun statusUpdatedSubscriber(event: StatusUpdatedEvent) {
-        logger.info("Status updated: {} with new color {}", event.newStatusName, event.newColor)
-    }
-}
+// package ru.quipy.projections
+//
+// import org.slf4j.Logger
+// import org.slf4j.LoggerFactory
+// import org.springframework.stereotype.Service
+// import ru.quipy.api.StatusCreatedEvent
+// import ru.quipy.api.StatusUpdatedEvent
+// import ru.quipy.streams.annotation.AggregateSubscriber
+// import ru.quipy.streams.annotation.SubscribeEvent
+//
+// @Service
+// @AggregateSubscriber(
+//     aggregateClass = StatusAggregate::class, subscriberName = "status-subs-stream"
+// )
+// class AnnotationBasedStatusEventsSubscriber {
+//
+//     val logger: Logger = LoggerFactory.getLogger(AnnotationBasedStatusEventsSubscriber::class.java)
+//
+//     @SubscribeEvent
+//     fun statusCreatedSubscriber(event: StatusCreatedEvent) {
+//         logger.info("Status created: {} with color {}", event.statusName, event.color)
+//     }
+//
+//     @SubscribeEvent
+//     fun statusUpdatedSubscriber(event: StatusUpdatedEvent) {
+//         logger.info("Status updated: {} with new color {}", event.newStatusName, event.newColor)
+//     }
+// }/
--- src/main/kotlin/ru/quipy/projections/TaskEventsSubscriber.kt
+++ src/main/kotlin/ru/quipy/projections/TaskEventsSubscriber.kt
@@ -1,41 +1,41 @@
-package ru.quipy.projections
-
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Service
-import ru.quipy.api.TaskAggregate
-import ru.quipy.api.TaskStatusChangedEvent
-import ru.quipy.api.TaskAssignedToUserEvent
-import ru.quipy.api.TaskCompletedEvent
-import ru.quipy.streams.AggregateSubscriptionsManager
-import javax.annotation.PostConstruct
-
-@Service
-class TaskEventsSubscriber {
-
-    val logger: Logger = LoggerFactory.getLogger(TaskEventsSubscriber::class.java)
-
-    @Autowired
-    lateinit var subscriptionsManager: AggregateSubscriptionsManager
-
-    @PostConstruct
-    fun init() {
-        subscriptionsManager.createSubscriber(TaskAggregate::class, "task-events-subscriber") {
-
-            `when`(TaskStatusChangedEvent::class) { event ->
-                logger.info("Task status changed: Task ID {} in project {} to new status {}",
-                    event.taskId, event.projectId, event.newStatusId)
-            }
-
-            `when`(TaskAssignedToUserEvent::class) { event ->
-                logger.info("Task assigned to user: Task ID {} in project {} assigned to user {}",
-                    event.taskId, event.projectId, event.assigneeId)
-            }
-
-            `when`(TaskCompletedEvent::class) { event ->
-                logger.info("Task completed: Task ID {} in project {}", event.taskId, event.projectId)
-            }
-        }
-    }
-}
+// package ru.quipy.projections
+//
+// import org.slf4j.Logger
+// import org.slf4j.LoggerFactory
+// import org.springframework.beans.factory.annotation.Autowired
+// import org.springframework.stereotype.Service
+// import ru.quipy.api.TaskAggregate
+// import ru.quipy.api.TaskStatusChangedEvent
+// import ru.quipy.api.TaskAssignedToUserEvent
+// import ru.quipy.api.TaskCompletedEvent
+// import ru.quipy.streams.AggregateSubscriptionsManager
+// import javax.annotation.PostConstruct
+//
+// @Service
+// class TaskEventsSubscriber {
+//
+//     val logger: Logger = LoggerFactory.getLogger(TaskEventsSubscriber::class.java)
+//
+//     @Autowired
+//     lateinit var subscriptionsManager: AggregateSubscriptionsManager
+//
+//     @PostConstruct
+//     fun init() {
+//         subscriptionsManager.createSubscriber(TaskAggregate::class, "task-events-subscriber") {
+//
+//             `when`(TaskStatusChangedEvent::class) { event ->
+//                 logger.info("Task status changed: Task ID {} in project {} to new status {}",
+//                     event.taskId, event.projectId, event.newStatusId)
+//             }
+//
+//             `when`(TaskAssignedToUserEvent::class) { event ->
+//                 logger.info("Task assigned to user: Task ID {} in project {} assigned to user {}",
+//                     event.taskId, event.projectId, event.assigneeId)
+//             }
+//
+//             `when`(TaskCompletedEvent::class) { event ->
+//                 logger.info("Task completed: Task ID {} in project {}", event.taskId, event.projectId)
+//             }
+//         }
+//     }
+// }
